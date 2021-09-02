1a725731211eb6d912539e77160da569ede46dca
yuvrajbhogle <bhogleyuvraj@yahoo.in>
2021-09-02 14:37:37 +0530

    Updates

---
+++ precache-manifest.8e824b8d27196e26434e2d675242308e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f9a07045034bd559632ef0763aa22db",
+    "url": "/Haptik-Assignment/index.html"
+  },
+  {
+    "revision": "5da0eab16b7883d0bd26",
+    "url": "/Haptik-Assignment/static/css/main.0220028c.chunk.css"
+  },
+  {
+    "revision": "c994a49bb648dec2d654",
+    "url": "/Haptik-Assignment/static/js/2.9be051d0.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/Haptik-Assignment/static/js/2.9be051d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da0eab16b7883d0bd26",
+    "url": "/Haptik-Assignment/static/js/main.a02bf8a8.chunk.js"
+  },
+  {
+    "revision": "8ac2e2ecd8003addb1c0",
+    "url": "/Haptik-Assignment/static/js/runtime-main.9d939cdb.js"
+  }
+]);
